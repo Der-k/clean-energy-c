6bbf4d83716f39472bd136ced2d46c18ddf379fd
Der-k <derekgk0@gmail.com>
2026-06-23 13:26:22 +0300

    Database updates

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function HomePage() {
     <HeroSection />
 
       <StatsBar />
-      <RoleEntrySection/>
+      
       <ProgrammeCtaSection />
        
       <ConferenceOverview />
